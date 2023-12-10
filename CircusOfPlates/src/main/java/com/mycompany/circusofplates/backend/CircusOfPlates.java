5b06508bd8e1ee515e4ac96e709c80908abc36b7
shrededlord <153372144+shrededlord@users.noreply.github.com>
2023-12-10 22:11:22 +0200

    asas

--- CircusOfPlates/src/main/java/com/mycompany/circusofplates/backend/CircusOfPlates.java
+++ CircusOfPlates/src/main/java/com/mycompany/circusofplates/backend/CircusOfPlates.java
@@ -6,6 +6,6 @@ package com.mycompany.circusofplates.backend;
 public class CircusOfPlates {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Worldvjjhgvhjgh!");
     }
 }
