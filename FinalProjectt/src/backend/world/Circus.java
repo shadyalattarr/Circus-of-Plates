af228a55069da4f7c774d72c3f42faed79133bf1
ShadiElattar <shadyalattar00@gmail.com>
2023-12-21 09:29:38 +0200

    spawns n per second... open to modification

--- FinalProjectt/src/backend/world/Circus.java
+++ FinalProjectt/src/backend/world/Circus.java
@@ -29,7 +29,7 @@ import eg.edu.alexu.csd.oop.game.World;
 public class Circus implements World {
     private static int MAX_TIME = 1 * 60 * 1000; // 1 minute
     private int score = 0;
-    private long endTime, startTime = System.currentTimeMillis();
+    private long endTime,timePassedInms = 0L, startTime = System.currentTimeMillis();
     private int RIGHT_STICK = 1;
     private int LEFT_STICK = 2;
     private final int width;
@@ -39,6 +39,7 @@ public class Circus implements World {
     private final List<GameObject> control;
     private final List<GameObject> objectsToFall;
     private int lives = 3;
+    private int i = 0;
     // for now difficulty
     DifficultyStrategy difficulty;
     MovementStrategy movement;
@@ -51,6 +52,229 @@ public class Circus implements World {
 
     // ------------
     private final Clown clown;
+
+    
+    // ------------
+
+    public Circus(int screenWidth, int screenHeight) {
+        this.width = screenWidth;
+        this.height = screenHeight;
+        constant = new LinkedList<GameObject>();
+        control = new LinkedList<GameObject>();
+        moving = new LinkedList<GameObject>();
+
+        // maybe difficulty sent in constructor?
+        intersection = new Intersection(this);
+        movement = new MovementStrategy(new NoOscillationStrategy(), new DownStrategy());
+        objFalling = new BombsStartegy();
+        difficulty = new DifficultyStrategy(new ObjectSpeedlvl2Strategy(), movement, objFalling);
+        // all above not here
+
+        clown = Clown.getInstance((int) (screenWidth / 2.6), (int) (screenHeight / 1.4),
+                "FinalProjectt\\clown-removebg-preview_3_53.png");
+
+        control.add(clown);
+        control.add(clown.getLeftStick());
+        control.add(clown.getRightStick());
+
+        // Iterator leftIterator = clownIterator.creatLeftIterable();
+        // Iterator rightIterator = clownIterator.creatRightIterable();
+        clownLeftIteratorConcrete = new ClownIteratorConcrete(clown.getLeftObjStack());
+        clownRightIteratorConcrete = new ClownIteratorConcrete(clown.getRightObjStack());
+
+        objectsToFall = objFalling.generateObjectsFalling(1000);
+
+        //moving = objectsToFall;
+        // every second
+        // moving.add(//objectsToFall)
+    }
+
+    public void spawn(int n)
+    {
+        for(int j =0;j<n;j++)
+        {
+            try{
+                System.out.println(j);
+                moving.add(objectsToFall.get(i+j));
+            }
+            catch(IndexOutOfBoundsException e)
+            {
+                System.out.println("ERRAOR : You ran out of falling objects");
+                //close game
+                System.exit(0);
+            }
+        }                   
+        i+=n;
+    }
+    @Override
+    public boolean refresh() {
+        boolean timeout = timePassedInms > MAX_TIME;
+        //before it was ex.6 sec passed.. if i just entered and 6 sec passed,, nothing
+        //but if now 7 sec passed a sec passed
+        if(timePassedInms/1000 + 1 == (System.currentTimeMillis() - startTime)/1000)
+        {
+            //System.out.println(timePassedInms/1000);
+            spawn(difficulty.getFallingObjectSpeedStrategy().getFallingObjectSpeed());
+        }
+        //update time passed
+        timePassedInms = System.currentTimeMillis() - startTime;
+
+            //boolean aSecondPassed = timePassedInms/1000;
+        GameObject lefttoIntersectWith;
+        GameObject righttoIntersectWith;
+        for (GameObject o : moving.toArray(new GameObject[moving.size()])) {
+            // before moving we check if it intersects with stick
+            lefttoIntersectWith = clown.getLeftObjStack().size() == 0 ? clown.getLeftStick()
+                    : clown.getLeftObjStack().peek();
+            righttoIntersectWith = clown.getRightObjStack().size() == 0 ? clown.getRightStick()
+                    : clown.getRightObjStack().peek();
+
+            if (o instanceof Plate)
+                intersection.setIntersection(new IntersectWithPlateStrategy());
+            else if (o instanceof Bomb)
+                intersection.setIntersection(new IntersectWithBombStrategy());
+
+            intersection.handleIntersection((FallingObject) o, righttoIntersectWith);
+            intersection.handleIntersection((FallingObject) o, lefttoIntersectWith);
+
+            // move
+            movement.move((FallingObject) o, difficulty.getFallingObjectSpeedStrategy());
+            checkLPLate(clownLeftIteratorConcrete);
+            checkRPLate(clownRightIteratorConcrete);
+
+            // do we want reuse?
+            // if (o.getY() >= getHeight()) {
+            //     // in bottom
+            //     // System.out.println("Plate hit bot");
+            //     reuse(o);
+            // }
+        }
+        GameObject stick = control.get(1);
+        // System.out.println("Stick x : " + stick.getX());
+        return !timeout;
+    }
+
+    public void reuse(GameObject object) {
+        object.setY(-1 * (int) (Math.random() * getHeight()));// get it up?
+        object.setX((int) (Math.random() * getWidth()));
+    }
+
+    public void checkLPLate(ClownIteratorConcrete iterator) {
+        Stack<GameObject> stack = getClown().getLeftObjStack();
+
+        if (iterator.getSize() >= 3) {
+            int i = 0;
+            iterator.setI(stack.size() - 1);
+            List<Plate> plates = new ArrayList<Plate>();
+
+            while (i <= 2) {
+
+                Plate plate = (Plate) iterator.next();
+                plates.add(plate);
+               // System.out.println(plate.getPlateColor());
+                i++;
+            }
+            i = 0;
+
+            if (plates.get(0).getPlateColor() == plates.get(1).getPlateColor()
+                    && plates.get(1).getPlateColor() == plates.get(2).getPlateColor()) {
+                score += 3;
+                while (i <= 2) {
+                    Plate plateRemoved = (Plate) stack.pop();
+                    getControlableObjects().remove(plateRemoved);
+                    i++;
+                }
+
+            }
+
+            plates.clear();
+            //System.out.println("size " + plates.size());
+            // if (iterator.getSize() >= 3) {
+            // Plate plate1 = (Plate) iterator.next();
+            // Plate plate2 = (Plate) iterator.next();
+            // }
+        }
+
+    }
+
+    public void checkRPLate(ClownIteratorConcrete iterator) {
+        Stack<GameObject> stack = getClown().getRightObjStack();
+
+        if (iterator.getSize() >= 3) {
+            int i = 0;
+            iterator.setI(stack.size() - 1);
+            List<Plate> plates = new ArrayList<Plate>();
+
+            while (i <= 2) {
+
+                Plate plate = (Plate) iterator.next();
+                plates.add(plate);
+              //  System.out.println(plate.getPlateColor());
+                i++;
+            }
+            i = 0;
+
+            if (plates.get(0).getPlateColor() == plates.get(1).getPlateColor()
+                    && plates.get(1).getPlateColor() == plates.get(2).getPlateColor()) {
+                score += 1;
+                while (i <= 2) {
+                    Plate plateRemoved = (Plate) stack.pop();
+                    getControlableObjects().remove(plateRemoved);
+                    i++;
+                }
+
+            }
+
+            plates.clear();
+          //  System.out.println("size " + plates.size());
+            // if (iterator.getSize() >= 3) {
+            // Plate plate1 = (Plate) iterator.next();
+            // Plate plate2 = (Plate) iterator.next();
+            // }
+        }
+
+    }
+
+    @Override
+    public int getSpeed() {
+        return 10;
+    }
+
+    @Override
+    public int getControlSpeed() {
+        return 10;
+    }
+
+    @Override
+    public List<GameObject> getConstantObjects() {
+        return constant;
+    }
+
+    @Override
+    public List<GameObject> getMovableObjects() {
+        return moving;
+    }
+
+    @Override
+    public List<GameObject> getControlableObjects() {
+        return control;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public String getStatus() {
+        return "Score=" + score + "   |   Time="
+                + Math.max(0, (/*MAX_TIME -*/ (System.currentTimeMillis() - startTime)) / 1000); // update status
+    }
 
     public int getScore() {
         return this.score;
@@ -132,197 +356,4 @@ public class Circus implements World {
         return this.clown;
     }
 
-    // ------------
-
-    public Circus(int screenWidth, int screenHeight) {
-        this.width = screenWidth;
-        this.height = screenHeight;
-        constant = new LinkedList<GameObject>();
-        control = new LinkedList<GameObject>();
-        // moving = new LinkedList<GameObject>();
-
-        // maybe difficulty sent in constructor?
-        intersection = new Intersection(this);
-        movement = new MovementStrategy(new NoOscillationStrategy(), new DownStrategy());
-        objFalling = new BombsStartegy();
-        difficulty = new DifficultyStrategy(new ObjectSpeedlvl2Strategy(), movement, objFalling);
-        // all above not here
-
-        clown = Clown.getInstance((int) (screenWidth / 2.6), (int) (screenHeight / 1.4),
-                "FinalProjectt\\clown-removebg-preview_3_53.png");
-
-        control.add(clown);
-        control.add(clown.getLeftStick());
-        control.add(clown.getRightStick());
-
-        // Iterator leftIterator = clownIterator.creatLeftIterable();
-        // Iterator rightIterator = clownIterator.creatRightIterable();
-        clownLeftIteratorConcrete = new ClownIteratorConcrete(clown.getLeftObjStack());
-        clownRightIteratorConcrete = new ClownIteratorConcrete(clown.getRightObjStack());
-
-        objectsToFall = objFalling.generateObjectsFalling(20);
-
-        moving = objectsToFall;
-        // every second
-        // moving.add(//objectsToFall)
-    }
-
-    @Override
-    public boolean refresh() {
-        boolean timeout = System.currentTimeMillis() - startTime > MAX_TIME;
-        GameObject lefttoIntersectWith;
-        GameObject righttoIntersectWith;
-        for (GameObject o : moving.toArray(new GameObject[moving.size()])) {
-            // before moving we check if it intersects with stick
-            lefttoIntersectWith = clown.getLeftObjStack().size() == 0 ? clown.getLeftStick()
-                    : clown.getLeftObjStack().peek();
-            righttoIntersectWith = clown.getRightObjStack().size() == 0 ? clown.getRightStick()
-                    : clown.getRightObjStack().peek();
-
-            if (o instanceof Plate)
-                intersection.setIntersection(new IntersectWithPlateStrategy());
-            else if (o instanceof Bomb)
-                intersection.setIntersection(new IntersectWithBombStrategy());
-
-            intersection.handleIntersection((FallingObject) o, righttoIntersectWith);
-            intersection.handleIntersection((FallingObject) o, lefttoIntersectWith);
-
-            // move
-            movement.move((FallingObject) o, difficulty.getFallingObjectSpeedStrategy());
-            checkLPLate(clownLeftIteratorConcrete);
-            checkRPLate(clownRightIteratorConcrete);
-
-            // want to make a method for that
-            if (o.getY() >= getHeight()) {
-                // in bottom
-                // System.out.println("Plate hit bot");
-                reuse(o);
-            }
-        }
-        GameObject stick = control.get(1);
-        // System.out.println("Stick x : " + stick.getX());
-        return !timeout;
-    }
-
-    public void reuse(GameObject object) {
-        object.setY(-1 * (int) (Math.random() * getHeight()));// get it up?
-        object.setX((int) (Math.random() * getWidth()));
-    }
-
-    @Override
-    public int getSpeed() {
-        return 10;
-    }
-
-    @Override
-    public int getControlSpeed() {
-        return 10;
-    }
-
-    @Override
-    public List<GameObject> getConstantObjects() {
-        return constant;
-    }
-
-    @Override
-    public List<GameObject> getMovableObjects() {
-        return moving;
-    }
-
-    @Override
-    public List<GameObject> getControlableObjects() {
-        return control;
-    }
-
-    @Override
-    public int getWidth() {
-        return width;
-    }
-
-    @Override
-    public int getHeight() {
-        return height;
-    }
-
-    @Override
-    public String getStatus() {
-        return "Score=" + score + "   |   Time="
-                + Math.max(0, (MAX_TIME - (System.currentTimeMillis() - startTime)) / 1000); // update status
-    }
-
-    public void checkLPLate(ClownIteratorConcrete iterator) {
-        Stack<GameObject> stack = getClown().getLeftObjStack();
-
-        if (iterator.getSize() >= 3) {
-            int i = 0;
-            iterator.setI(stack.size() - 1);
-            List<Plate> plates = new ArrayList<Plate>();
-
-            while (i <= 2) {
-
-                Plate plate = (Plate) iterator.next();
-                plates.add(plate);
-               // System.out.println(plate.getPlateColor());
-                i++;
-            }
-            i = 0;
-
-            if (plates.get(0).getPlateColor() == plates.get(1).getPlateColor()
-                    && plates.get(1).getPlateColor() == plates.get(2).getPlateColor()) {
-                score += 3;
-                while (i <= 2) {
-                    Plate plateRemoved = (Plate) stack.pop();
-                    getControlableObjects().remove(plateRemoved);
-                    i++;
-                }
-
-            }
-
-            plates.clear();
-            //System.out.println("size " + plates.size());
-            // if (iterator.getSize() >= 3) {
-            // Plate plate1 = (Plate) iterator.next();
-            // Plate plate2 = (Plate) iterator.next();
-            // }
-        }
-
-    }
-
-    public void checkRPLate(ClownIteratorConcrete iterator) {
-        Stack<GameObject> stack = getClown().getRightObjStack();
-
-        if (iterator.getSize() >= 3) {
-            int i = 0;
-            iterator.setI(stack.size() - 1);
-            List<Plate> plates = new ArrayList<Plate>();
-
-            while (i <= 2) {
-
-                Plate plate = (Plate) iterator.next();
-                plates.add(plate);
-              //  System.out.println(plate.getPlateColor());
-                i++;
-            }
-            i = 0;
-
-            if (plates.get(0).getPlateColor() == plates.get(1).getPlateColor()
-                    && plates.get(1).getPlateColor() == plates.get(2).getPlateColor()) {
-                score += 1;
-                while (i <= 2) {
-                    Plate plateRemoved = (Plate) stack.pop();
-                    getControlableObjects().remove(plateRemoved);
-                    i++;
-                }
-
-            }
-
-            plates.clear();
-          //  System.out.println("size " + plates.size());
-            // if (iterator.getSize() >= 3) {
-            // Plate plate1 = (Plate) iterator.next();
-            // Plate plate2 = (Plate) iterator.next();
-            // }
-        }
-
-    }
 }
--- FinalProjectt/src/backend/world/ObjectsFallingStrategy/PlatesOnlyStrategy.java
+++ FinalProjectt/src/backend/world/ObjectsFallingStrategy/PlatesOnlyStrategy.java
@@ -2,15 +2,17 @@ package backend.world.ObjectsFallingStrategy;
 
 import java.util.ArrayList;
 import eg.edu.alexu.csd.oop.game.GameObject;
+import backend.object.ObjectFactory;
 import backend.object.Plate;
 
 public class PlatesOnlyStrategy implements ObjectsFallingStrategy{
     ArrayList<GameObject> moving = new ArrayList<GameObject>();
+    ObjectFactory factory = new ObjectFactory();
 
     @Override
     public ArrayList<GameObject> generateObjectsFalling(int n) {
         for(int i=0; i < n; i++)
-            moving.add(new Plate((int)(Math.random()*1400/*getWidth()*/), (int)(Math.random()*750/*getHeight()*//2)));
+            moving.add(factory.createObject("Plate",(int)(Math.random()*1400/*getWidth()*/), (int)(Math.random()*750/10 /*getHeight()/2*/),null));
         return moving;
     }
 
